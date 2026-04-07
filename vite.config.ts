f0dc4b67903c1162c2342433271f41d358b59e9d
alt9874 <jsj20210104@gmail.com>
2026-04-07 14:43:45 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,6 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  base: '/KIDS_GAME/'
-})
-
-
-
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
